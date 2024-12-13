e4ba8bb3197633483c0ef75fde1693818f34d59c
Isaac Ireland <isaac.ireland@ii.co.uk>
2024-12-13 16:49:17 +0000

    tidy up

--- advent-4-1/src/main.rs
+++ advent-4-1/src/main.rs
@@ -1,4 +1,3 @@
-use std::array;
 use std::collections::HashMap;
 use std::fs::File;
 use std::io::{self, BufRead, BufReader};
@@ -8,10 +7,10 @@ fn main() -> io::Result<()> {
     // Step 1: Create a HashMap to represent the grid
     let mut grid: HashMap<(i32, i32), String> = HashMap::new();
     // Open the file
-    let file = File::open("test_input.txt")?;
+    let file = File::open("input.txt")?;
     let reader = BufReader::new(file);
     let valid_path = ["X","M","A","S"];
-    let mut answer_array: Vec<i32> = vec![0];
+    let mut answer_array: Vec<i32> = vec![];
 
     // Process each line in the file
     let mut index: i32 = 0;
@@ -31,8 +30,10 @@ fn main() -> io::Result<()> {
         index = index + 1;
         col = 0;
     }
-    parse_grid(grid, valid_path);
-
+    parse_grid(&grid, valid_path, &mut answer_array);
+    
+    let total: i32 = answer_array.iter().sum();
+    println!("Total Matches {:?}", total);
     Ok(())
 }       
 
@@ -52,99 +53,69 @@ fn get_neighbours(target: (i32,i32)) -> [(i32, i32); 8] {
     // answer_array.push(0)
 }
 
-fn parse_grid(grid: HashMap<(i32, i32), String>, valid_path: [&str; 4]) {
-    for (key, value) in &grid {   
+fn parse_grid(grid: &HashMap<(i32, i32), String>, valid_path: [&str; 4], answer_array: &mut Vec<i32>) {
+    for (key, value) in grid {   
         let origin: &(i32, i32) = &key.clone();   
         println!("Origin: {:?}", origin);  
         if value == valid_path[0] {
-            follow_path(*origin, valid_path, grid.clone());
+            follow_path(*origin, valid_path, grid, 1, answer_array);
         }
     }
 }
 
-fn follow_path (origin: (i32, i32), valid_path: [&str; 4], grid: HashMap<(i32, i32), String>) {
+fn follow_path (origin: (i32, i32), valid_path: [&str; 4], grid: &HashMap<(i32, i32), String>, position: usize, answer_array: &mut Vec<i32>) {
     let t_neighbours = get_neighbours(origin);
-    for i in t_neighbours {
-        println!("neighbour: {:?}",i);
-        if let Some(value) = grid.get(&i) {
-            if value == valid_path[1] {
-                let path: [(i32, i32); 4];
-                let dif: (i32, i32);
-                dif = (
-                    origin.0 - i.0,
-                    origin.1 - i.1
+    println!("neighbours {:?}", t_neighbours);
+    for neighbour in t_neighbours {
+        println!("neighbour: {:?}",neighbour);
+        if let Some(value) = grid.get(&neighbour) {
+            if *value == valid_path[position] {
+                println!("current_value {}",value);
+                let dir: (i32, i32) = (
+                    neighbour.0 - origin.0,
+                    neighbour.1 - origin.1
                 );
-                // println!("diff: {:?}", dif);
-                // let remaining_path_len: &usize = &valid_path[2..].len();
-                // let path_locations: [(i32,i32); remaining_path_len];
-                // for p in &valid_path[2..] {
-                    
-                // }
-                // for p in &valid_path[2..] {
-                    
-                // }
+                follow_path_direction(origin, dir, valid_path, grid, answer_array);
             }
         } else {
-            println!("No value found for neighbour: {:?}", i);
+            println!("No value found for neighbour: {:?}", neighbour);
         }
-        // for p in valid_path {
-        //     let dir: i32 = origin - i
-        // }
     }
 }
 
-fn get_paths(origin: (i32,i32), size: i32) -> Vec<i32> {
-    let t_origin: (i32, i32) = origin.clone();
-    let n_path: (i32, i32);
-    let diff: (i32, i32);
-    diff = (
-        origin.0 - origin.0,
-        origin.1 - origin.1
-    );
-    println!("diff: {:?}", diff);
-    for i in 1..size-1 {
-        n_path = (
-            origin.0 + diff.0,
-            origin.1 + diff.1
-        )
+fn follow_path_direction(origin: (i32, i32), dir: (i32,i32), valid_path: [&str; 4], grid: &HashMap<(i32, i32), String>, answer_array: &mut Vec<i32>) {
+    let mut locations: Vec<(i32,i32)> = vec![];
+    locations.push(origin);
+    println!("Pushing Origin: {:?}",origin);
+    let mut new_location: (i32, i32) = origin;
+    let mut new_location_answers: Vec<Option<&String>> = vec![];
+
+    for i in 1..valid_path.len() {
+        new_location = (new_location.0 + dir.0, new_location.1 + dir.1);
+        locations.push(new_location);
+        
     }
-    return 
+    println!("Locations: {:?}",locations);
+    for loc in &locations {
+        // Push the result of grid.get() into new_location_answers
+        new_location_answers.push(grid.get(loc));
+    }
+    println!("Checking locations {:?}",new_location_answers);
+
+    for (index, value) in new_location_answers.iter().enumerate() {
+        println!("index {:?}, value {:?}", index, value);
+        println!("valid path index {:?}", valid_path[index]);
+
+        if Some(String::from(valid_path[index])) == value.as_deref().cloned() {
+            println!("its a match!");
+        }
+        else {
+            break
+        }
+        
+        if value.as_deref() == Some(&String::from(valid_path[valid_path.len()-1])) && index == valid_path.len()-1{
+            println!("last item!");
+            answer_array.push(1);
+        }
+    }
 }
-// fn map_dir(dir: (i32,i32)) -> [(i32,i32); 4] {
-//     let targets 
-
-//     return targets
-// }
-
-//     for i in neighbours
-//     let origin = (0, 0);
-//     get_neighbours(target);
-//     for &(row, col) in &neighbors {
-//         if let Some(&value) = grid.get(&(row, col)) {
-//             println!("Neighbor at ({}, {}): {}", row, col, value);
-//         } else {
-//             println!("Neighbor at ({}, {}): None", row, col);
-//         }
-//     }
-// }
-
-// fn valid_path(previous_path: str, valid_path: str, ) {
-//     if previous_path == valid_path {
-//         return true;
-//     }
-//     return false;
-// }
-
-// fn follow_path (&origin: ) {
-
-//     for i in neighbours
-//     let origin = (0, 0);
-//     get_neighbours(target);
-//     for &(row, col) in &neighbors {
-//         if let Some(&value) = grid.get(&(row, col)) {
-//             println!("Neighbor at ({}, {}): {}", row, col, value);
-//         } else {
-//             println!("Neighbor at ({}, {}): None", row, col);
-//         }
-//     }
-// }
